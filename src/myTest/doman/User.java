4e809f4490c711d803a3526aa2ce0964cdaf55b9
f <2710022878@qq.com>
2019-06-16 21:43:52 +0800

    myTest secended commit

--- src/myTest/doman/User.java
+++ src/myTest/doman/User.java
@@ -5,4 +5,22 @@ public class User {
 	private String name;
 	private Integer age;
 	private Integer gender;
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setAge(Integer age){
+		this.age = age;
+	}
+	public Integer getAge(){
+		return age;
+	}
+	public void setGender(Integer gender){
+		this.gender = gender;
+	}
+	public Integer getGender(){
+		return gender;
+	}
 }
